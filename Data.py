5699e5ca7cb43b36b8ef843922ef7707f1c442d8
compnuclearuefs <166620513+compnuclearuefs@users.noreply.github.com>
2025-04-01 17:40:23 -0300

    Interconexão entre Google Drive e Programa de Visualização
    Nesse commit, foi aprimorado o download do arquivo de experimento .lbsl. O objetivo é fazer com que se possa inserir um link de qualquer experimento no prórpio Dashboard. Nossa etapa agora é:
    
    - Fazer no Dash uma barra para carregar o link;
    - Trabalhar em cima dos erros dos usuários;
    - Tornar a lógica de verificação mais clara;

--- Data.py
+++ Data.py
@@ -1,5 +1,8 @@
 from datetime import datetime, timedelta
 import requests
+import gdown
+
+FIRST_STRING = 'Acquisition time\tNeg Width   \tFall time   \tRise time   \tPk-Pk'
 
 class ExperimentData:
 
@@ -25,36 +28,94 @@ class ExperimentData:
 
         return date    
 
-    def processData (self):
+    def verifyLink (self, url, file_id):
 
-        url = "https://drive.google.com/uc?id=1fgm_zJTyqmJ5bi1XGLnPVjLpCt_eeMjU&export=download"
+        try:
+            # Extrair o ID do arquivo do link compartilhado
+            #file_id = url.split('/d/')[1].split('/')[0]
+            metadata_url = f"https://drive.google.com/uc?export=download&id={file_id}"
+
+            # Realizar uma requisição para obter as informações do arquivo
+            response = requests.get(metadata_url, stream=True)
+
+            # Verificar se a resposta está correta
+            if "text/html" in response.headers.get("Content-Type", ""):
+                print("Erro: O arquivo não está acessível ou o link está incorreto.")
+                return None
+            
+            # Pegar o nome do arquivo dos cabeçalhos
+            content_disposition = response.headers.get("Content-Disposition", "")
+            if "filename=" in content_disposition:
+                filename = content_disposition.split("filename=")[1].strip('"')
+                print(f"Nome do arquivo: {filename}")
+                
+                # Verificar extensão .lbsl
+                if filename.lower().endswith(".lbsl"):
+                    print("O arquivo é do tipo .lbsl.")
+                    return filename
+                else:
+                    print("O arquivo não é do tipo .lbsl.")
+                    return None
+            else:
+                print("Não foi possível obter o nome do arquivo.")
+                return None
+
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
+            return None
+
+        #url = "https://drive.google.com/drive/folders/1D80XAHGkujFCqzpMmobuVD-3S9bDS6IV?usp=sharing"
         response = requests.get(url)
-        with open("dataEDT.lbsl", "wb") as file:
-            file.write(response.content)
-    
-        filepath = "dataEDT.lbsl"
 
-        with open(filepath, 'r') as file:
-            # Ignorar a primeira linha
-            next(file)
-            for line in file:
-                
-                # Separar os valores na linha por espaço ou tabulação
-                values = line.split()
-                
-                # Adicionar os valores nas listas apropriadas
-                if (values [0].find(',') > -1):
-                    change = values [0]
-                    values[0] = change.replace(",", ".")
+        if (FIRST_STRING in response.content):
+            with open("data.lbsl", "wb") as file:
+                file.write(response.content)
+            return True
+        else:
+            return False
+
+        
+
+    def processData (self, url):
+        FILE_PATH = "data.lbsl"
+        confirm = False
+        #url = "https://drive.google.com/uc?id=1fgm_zJTyqmJ5bi1XGLnPVjLpCt_eeMjU&export=download"
+        #response = requests.get(url)
+        #url = "https://drive.google.com/file/d/1lQNHwqzCv706MYDySggn9Vn6J1wiA-om/view?usp=drive_link"
+        url = "https://docs.google.com/spreadsheets/d/0B7N1fAzIqCLHT2FZTU1zWkdLNE0/edit?usp=drive_link&ouid=102395208117010915304&resourcekey=0-Jgwv5mx8hAM1ZTxAFeva9w&rtpof=true&sd=true"
+        
+        if ('/d/' in url and self.verifyLink (url, file_id)):
+            file_id = url.split('/d/')[1].split('/')[0]
+            #confirm = self.verifyLink (url)
+             
+            gdown.download(f"https://drive.google.com/uc?id={file_id}", FILE_PATH, quiet=False)
+            #with open("data.lbsl", "wb") as file:
+            #    file.write(response.content)
+        
+            with open(FILE_PATH, 'r') as file:
+                # Ignorar a primeira linha
+                next(file)
+                for line in file:
                     
+                    # Separar os valores na linha por espaço ou tabulação
+                    values = line.split()
+                    
+                    # Adicionar os valores nas listas apropriadas
+                    if (values [0].find(',') > -1):
+                        change = values [0]
+                        values[0] = change.replace(",", ".")
+                        
 
-                values[0] = float(values[0]) + 2451545 # CONVERSÃO PARA DATA REAL, ALTERAÇÃO ESPECÍFICA DO PROGRAMA
-                self.acqTimeJul.append(values[0])
-                self.negWidht.append(float(values[1]))
-                self.fallTime.append(float(values[2]))
-                self.riseTime.append(float(values[3]))
-                self.pkPk.append(float(values[4]))
+                    values[0] = float(values[0]) + 2451545 # CONVERSÃO PARA DATA REAL, ALTERAÇÃO ESPECÍFICA DO PROGRAMA
+                    self.acqTimeJul.append(values[0])
+                    self.negWidht.append(float(values[1]))
+                    self.fallTime.append(float(values[2]))
+                    self.riseTime.append(float(values[3]))
+                    self.pkPk.append(float(values[4]))
 
-                
-                gregorianDate = self.julianToGregorian(float(values[0]))
-                self.acqTimeGreg.append(gregorianDate)
+                    
+                    gregorianDate = self.julianToGregorian(float(values[0]))
+                    self.acqTimeGreg.append(gregorianDate)
+            confirm = True
+
+        return confirm
